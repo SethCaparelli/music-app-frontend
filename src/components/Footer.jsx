f12ee27e412546ac76ee16c79335435195705c1a
Seth Caparelli <sethcaparelli@gmail.com>
2018-03-22 16:12:47 -0600

    footer style linked

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,9 @@ class Footer extends Component {
     render(){
         return(
             <footer>
-                <small className="footer-link" onClick={() => window.location = "https://seth-caparelli.com"}>&copy; Seth Caparelli 2018</small>
-                <h4 className="footer-link" onClick={() => window.location = "https://cocktail.surge.sh"}>Cocktail=></h4>
+                <h4>&copy; Seth Caparelli 2018</h4>
+                <h4><a className="footer-link" target="_blank" rel="noopener noreferrer" href="https://cocktail.surge.sh">Cocktail=></a></h4>
+                <h4><a className="footer-link" target="_blank" rel="noopener noreferrer" href="https://seth-caparelli.com">Seth-Caparelli.com</a></h4>
             </footer>
         )
     }
