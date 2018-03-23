15db19397d970f156183eca74437697cb6996456
Seth Caparelli <sethcaparelli@gmail.com>
2018-03-23 15:39:30 -0600

    build---> ready to deploy

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -5,7 +5,6 @@ class Event extends Component {
     render(){
         const tourInfo = this.props.tourInfo
         const date = tourInfo.datetime
-        console.log(tourInfo.url)
         return(
             <div id="tour">
                 <div id="name">
@@ -27,39 +26,3 @@ class Event extends Component {
 }
 
 export default Event
-
-
-//city:
-// "Monterrey"
-// country:
-// "Mexico"
-// latitude:
-// "25.6768703"
-// longitude:
-// "-100.2899933"
-// name:
-// "Parque Fundidora"
-// region:
-// "NL"
-
-// :
-// {…}
-// artist_id:
-// "13054608"
-// datetime:
-// "2018-03-10T14:00:00"
-// description:
-// "Beyond Wonderland Monterrey"
-// id:
-// "21485119"
-// lineup:
-// Array[1]
-// offers:
-// Array[1]
-// on_sale_datetime:
-// "2017-11-06T17:00:00"
-// url:
-// "https://www.bandsintown.com/e/21485119?app_id=Music.at&came_from=267"
-// venue:
-// {…}
-
