a6947d7943243ec5d89816a9386cc8fbf7f8a251
Seth Caparelli <sethcaparelli@gmail.com>
2018-03-23 08:57:36 -0600

    icon style set

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -6,6 +6,9 @@ class Splash extends Component {
             <div id="splash-body">
                 <img id="splash-logo" src="/assets/music@-icon.png" alt="icon"/>
                 <h1 id="splash-header">Music@</h1>
+                <p>This app presents users their followed spotify artists and connects them to the Bandsintown API
+                    so that they can see their favorite artist's tour schedule. Users are also able to search and play other artists.
+                </p>
                 <button
                     onClick={() => window.location = "http://localhost:8888/login"/*"https://blooming-gorge-11355.herokuapp.com/login"*/}
                     id="splash-button">
